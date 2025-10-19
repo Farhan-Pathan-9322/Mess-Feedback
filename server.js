4d2b7ac9572c3030c1cca7b8694b2405d4d545d6
Farhan Pathan <148573533+Farhan-Pathan-9322@users.noreply.github.com>
2025-10-19 17:12:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const allowedOrigins = [
   "http://localhost:5173"
 ];
 app.use(cors({
+  
   origin: (origin, cb) => {
     
     if (!origin || allowedOrigins.includes(origin)) return cb(null, true);
